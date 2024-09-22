ef88051d20059b624d7d5ce6600209c7ccc54852
Hayden Curfman <hcurfman@keemail.me>
2024-09-22 14:30:09 -0400

    Initial Commit

---
+++ LiveCodeDemo/LiveCodeDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiveCodeDemoApp.swift
+//  LiveCodeDemo
+//
+//  Created by Hayden Curfman on 9/22/24.
+//
+
+import SwiftUI
+
+@main
+struct LiveCodeDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
